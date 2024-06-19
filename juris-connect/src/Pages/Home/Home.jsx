fc2c50ab78461bd8009dd2d4ec3810b7c003a496
Tharindu-Lakmal <126395249+Tharindu-Lakmal@users.noreply.github.com>
2024-06-20 01:09:00 +0530

    initial files are added

---
+++ juris-connect/src/Pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+import Navbar from '../../components/Navbar/Navbar'
+
+const Home = () => {
+  return (
+    <div className='home'>
+      <h2>Home Page</h2>
+
+      <Navbar />
+    </div>
+  )
+}
+
+export default Home
---
+++ juris-connect/src/Pages/Lawyers/Lawyers.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Lawyers.css'
+
+const Lawyers = () => {
+  return (
+    <div className='lawyers'>
+        <h2>Lawyers Page</h2>
+    </div>
+  )
+}
+
+export default Lawyers
