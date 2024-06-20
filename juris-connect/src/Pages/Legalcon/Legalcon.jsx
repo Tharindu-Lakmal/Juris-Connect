9147d1057b0f4b61311d56a7d38fcbe360b38641
Sasindu Fernando <pujana.sasindu@gmail.com>
2024-06-21 04:25:34 +0530

    create nav bar

---
+++ juris-connect/src/Pages/Legalcon/Legalcon.jsx
@@ -0,0 +1,77 @@
+import Navbar from '../../components/Header/Navbar'
+import "./Legalcon.css"
+import img1 from "../../Assets/Legalcon.jpeg"
+// import '@fortawesome/fontawesome-free/css/all.min.css';
+import Footer from '../../components/Footer/Footer';
+
+
+export default function Legalcon(){
+    return(
+    <div>
+      <Navbar />
+      <div className="content">
+        <div className="left">
+          <h1>Get the legal help <br/> you need</h1>
+          <p>Every problem is unique, and we offer personalized <br />recomendations to suit your specific needs</p>
+          <div className='buttons'>
+          <button className='button-left'>Ask your legal question</button>
+          <button className='button-right'>Free legal advice</button>
+          </div>
+        </div>
+        <div className="right">
+          <img src={img1} alt="Description" />
+        </div>
+      </div>
+      <section>
+        <form>
+          <h2> Describe your situation</h2>
+          <h3>Ready to submit your question? Here's what we need from you</h3>
+          <div className='box'> 
+              <div className='input-box'>
+                <label>Heading</label>
+                <input type='text' className='field' placeholder='Enter your name' required/>
+
+              </div>
+              <div className='input-box'>
+                <label>Topic</label>
+                <input type='text' className='field' placeholder='Choose one' required/>
+
+              </div>
+              <div className='input-box'>
+                <label>Home town</label>
+                <input type='text' className='field' placeholder='Nearest city belong to this question' required/>
+
+              </div>
+               <div className='input-box'>
+                  <label>Description</label>
+                  <input type='text' className='field mess' placeholder='Briefly describe your question/problem' required/>
+                
+              </div>
+              <div className="warning-box">
+                 <i className="fas fa-exclamation-triangle"> Note</i>
+                 <p>JurisConect will not be responsible in case of any issues or discrepancies with the expert answer. All free questions will publish in the site for public knowledge sharing. You should not include any person or organization name and detail in the question which would affect the brand or reputation of the other party.</p>
+              </div>
+
+              <div className="checkbox-container">
+                   <input type="checkbox" id="agree" name="agree" />
+                   <label htmlFor="agree">I have read the notice and i hold the responsibility for the details provided here.</label>
+              </div>
+               <div className='button'>
+               <button className='button-submit'>Submit</button>
+               <button className='button-cancel'>Cancel</button>
+              </div>
+          </div>
+          
+        </form>
+        
+      </section>
+      
+            <Footer />
+          
+      
+      
+    </div>
+    
+
+    )
+}
---
+++ juris-connect/src/components/Header/Navbar.jsx
@@ -0,0 +1,37 @@
+import React, {useState} from "react";
+import "./Navbar.css";
+
+function Navbar() {
+    const [active, setActive] = useState("nav_menu");
+    const [toggleIcon, setToggleIcon] = useState("nav_toggler");
+
+    const navToggle = () => {
+        active === 'nav_menu' ? setActive('nav_menu nav_active') : setActive('nav_menu');
+
+        toggleIcon === "nav_toggler" ? setToggleIcon("nav_toggler toggle") : setToggleIcon("nav_toggler");
+    };
+
+    return (
+        <nav className="nav">
+            <a href="#" className="nav_brand"><span>Juri</span>Connect</a>
+            <ul className={active}>
+                <li className="nav_item"><a href="Home" className="nav_link">Home</a></li>
+                <li className="nav_item"><a href="Legalcon" className="nav_link">Community Forum</a></li>
+                <li className="nav_item"><a href="About" className="nav_link">About Us</a></li>
+            </ul>
+            <button className="login_btn">Login</button>
+            
+            <div onClick = {navToggle} className={toggleIcon}>
+                    <div className="line1"></div>
+                    <div className="line2"></div>
+                    <div className="line3"></div>
+            
+            </div>
+            
+            
+        </nav>
+    );
+    
+}
+
+export default Navbar;
