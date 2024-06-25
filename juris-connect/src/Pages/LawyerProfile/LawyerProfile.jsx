961ef3e73bfb575489304f2cb1db87aca4d6e040
Sasindu Fernando <pujana.sasindu@gmail.com>
2024-06-25 19:14:31 +0530

    lawyer profile page created

--- juris-connect/src/Pages/LawyerProfile/LawyerProfile.jsx
+++ juris-connect/src/Pages/LawyerProfile/LawyerProfile.jsx
@@ -4,6 +4,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import Footer from '../../components/Footer/Footer';
 import Navbar from '../../components/Header/Navbar';
+import img1 from "../../assets/lawyer_pic.jpg";
+import img2 from "../../assets/law_page_back.jpg";
+
 
 
 function LawyerProfile() {
@@ -11,11 +14,16 @@ function LawyerProfile() {
     <div> 
         <Navbar />
     <div className="profile-container">
+        <div>
+            <img src={img2} className='profile-cover' />
+        </div>
             <div className="profile-header">
-                <img src="your-profile-image-url.jpg" alt="Lawyer" className="profile-image" />
+                <img src={img1} alt="Lawyer" className="profile-image" />
                 <h2>Lawyer Kamalsiri Perera</h2>
-                <p className="verified"><i className="fas fa-check-circle"></i> Verified</p>
+                <p className="verified"> Verified&nbsp;<i className="fas fa-check-circle"></i> </p>
             </div>
+        
+            
             <div className="profile-consulting-options">
                 <div className="option">
                     <i className="fas fa-phone"></i>
