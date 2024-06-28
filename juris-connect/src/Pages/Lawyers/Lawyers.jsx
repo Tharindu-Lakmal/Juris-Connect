f9300ddb6eaa5cebbcd714a48d99a4c9992f6394
Tharindu-Lakmal <126395249+Tharindu-Lakmal@users.noreply.github.com>
2024-06-28 14:26:17 +0530

    lawyer page files and images added and hero and search bar created

--- juris-connect/src/Pages/Lawyers/Lawyers.jsx
+++ juris-connect/src/Pages/Lawyers/Lawyers.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import './Lawyers.css'
+import { Link } from 'react-router-dom'
+import LawyerHero from '../../components/Lawyers/LawyerHero/LawyerHero'
+import LawyerSearch from '../../components/Lawyers/LawyerSearch/LawyerSearch'
 
 const Lawyers = () => {
   return (
     <div className='lawyers'>
-        <h2>Lawyers Page</h2>
+        <LawyerHero />
+        <LawyerSearch />
     </div>
   )
 }
