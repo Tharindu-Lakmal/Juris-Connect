7b09ddd98384c18dde3a916d21d60cffa8e305ad
Tharindu-Lakmal <126395249+Tharindu-Lakmal@users.noreply.github.com>
2024-06-28 14:35:32 +0530

    lawyer cards component added

--- juris-connect/src/Pages/Lawyers/Lawyers.jsx
+++ juris-connect/src/Pages/Lawyers/Lawyers.jsx
@@ -3,12 +3,14 @@ import './Lawyers.css'
 import { Link } from 'react-router-dom'
 import LawyerHero from '../../components/Lawyers/LawyerHero/LawyerHero'
 import LawyerSearch from '../../components/Lawyers/LawyerSearch/LawyerSearch'
+import LawyerCard from '../../components/Lawyers/LawyerCard/LawyerCard'
 
 const Lawyers = () => {
   return (
     <div className='lawyers'>
         <LawyerHero />
         <LawyerSearch />
+        <LawyerCard />
     </div>
   )
 }
